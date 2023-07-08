0f53519f4d50343eb5755e90080d7c7c318fd7c6
文孝礼 <873619879@qq.com>
2023-07-08 11:00:27 +0800

    feat: vuessr

---
+++ myApp/ssr/example.js
@@ -0,0 +1,13 @@
+// 此文件运行在 Node.js 服务器上
+import { createSSRApp } from 'vue'
+// Vue 的服务端渲染 API 位于 `vue/server-renderer` 路径下
+import { renderToString } from 'vue/server-renderer'
+
+const app = createSSRApp({
+  data: () => ({ count: 1 }),
+  template: `<button @click="count++">{{ count }}</button>`
+})
+
+renderToString(app).then((html) => {
+  console.log(html)
+})
