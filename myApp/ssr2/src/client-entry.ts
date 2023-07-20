5baf7b344bcb714456e276a22d41ef4e99ea6432
文孝礼 <873619879@qq.com>
2023-07-20 14:41:50 +0800

    feat: 代码优化更新

--- myApp/ssr2/src/client-entry.ts
+++ myApp/ssr2/src/client-entry.ts
@@ -1,11 +1,10 @@
 import { createApp } from './main.js'
 const { app, router,pinia } = createApp()
 
-
 // 拿到服务端的状态数据对客户端进行注水
-if (window.__pinia__) {
-    pinia.state.value = window.__pinia__
-}
+// if (window.__pinia__) {
+//     pinia.state.value = window.__pinia__
+// }
 
 router.isReady().then(() => {
     app.mount('#app')
--- myApp/ssr2/src/server-entry.ts
+++ myApp/ssr2/src/server-entry.ts
@@ -3,10 +3,12 @@ import { renderToString } from 'vue/server-renderer'
 
 export async function render(url:string) {
   const { app, router, pinia  }  = createApp()
-  // 这里需要等待路由加载完成才能渲染
+  
   app.__data__ = {}
   await router.push(url)
   await router.isReady()
+
+  // 这里需要等待路由加载完成才能渲染, 以保证服务端的数据已经请求完毕
   const html = await renderToString(app)
   
   return {
